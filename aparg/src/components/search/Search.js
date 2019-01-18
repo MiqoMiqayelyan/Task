e5ae0d44725ce6f0ab80379bcf62fb7da815e626
MiqoMiqayelyan <miqo.miqayelyan@mail.ru>
2019-01-18 21:14:27 +0400

    make search component

--- aparg/src/components/search/Search.js
+++ aparg/src/components/search/Search.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Search = function(props){
     return(
-        <form onSubmit={props.getPhotoName}>
+        <form onSubmit={props.getFirstImgUrls}> 
             <input type='txt' placeholder='finde your photo' />
             <input className='submit' type='submit' value='Search'/>
         </form>
